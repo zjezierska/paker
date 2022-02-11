e8c45386bbea98fa5072976784caf55e1a56a0d8
zjezierska <z.jezierska2@student.uw.edu.pl>
2022-02-11 16:35:47 +0100

    Początek modułów

--- paker/main.cpp
+++ paker/main.cpp
@@ -5,8 +5,11 @@
 //  Created by Zuzanna Jezierska on 11/02/2022.
 //
 
+#include <Python.h>
 #include <iostream>
 #include <cmath>
+
+using namespace std;
 
 const double e = 2.7182818284590452353602874713527;
 
@@ -18,12 +21,34 @@ double cosh_impl(double x) {
     return (1 + pow(e, (-2 * x))) / (2 * pow(e, -x));
 }
 
-double tanh_impl(double x) {
-    return sinh_impl(x) / cosh_impl(x);
+PyObject* tanh_impl(PyObject* /* unused module reference */, PyObject* o) {
+    double x = PyFloat_AsDouble(o);
+    double tanh_x = sinh_impl(x) / cosh_impl(x);
+    return PyFloat_FromDouble(tanh_x);
 }
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+static PyMethodDef superfastcode_methods[] = {
+    // The first property is the name exposed to Python, fast_tanh
+    // The second is the C++ function with the implementation
+    // METH_O means it takes a single PyObject argument
+    { "fast_tanh", (PyCFunction)tanh_impl, METH_O, nullptr },
+
+    // Terminate the array with an object containing nulls.
+    { nullptr, nullptr, 0, nullptr }
+};
+
+static PyModuleDef superfastcode_module = {
+    PyModuleDef_HEAD_INIT,
+    "superfastcode",                        // Module name to use with Python import statements
+    "Provides some functions, but faster",  // Module description
+    0,
+    superfastcode_methods                   // Structure that defines the methods of the module
+};
+
+PyMODINIT_FUNC PyInit_superfastcode() {
+    return PyModule_Create(&superfastcode_module);
+}
+
+int main() {
     return 0;
 }
