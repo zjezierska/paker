949444ccba7f8a492b2fc9987cf48fffc09fa904
zjezierska <z.jezierska2@student.uw.edu.pl>
2022-02-11 15:23:29 +0100

    Initial Commit

---
+++ paker/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  paker
+//
+//  Created by Zuzanna Jezierska on 11/02/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
